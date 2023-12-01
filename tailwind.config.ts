6721614d033d1a0bc1008c64226d09d0af463cb3
Vítor <caulicons@hotmail.com>
2023-12-01 08:13:30 -0300

    style(design system): adding variable colors in tailwind and global css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: 'rgba(var(--primary), <alpha-value>)',
+        secondary: 'rgba(var(--secondary), <alpha-value>)',
+        black: 'rgba(var(--black), <alpha-value>)',
+        white: 'rgba(var(--white), <alpha-value>)',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
