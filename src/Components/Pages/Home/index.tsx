38814981c30f096627fad40383ad29d05c624c29
Vítor <caulicons@hotmail.com>
2023-12-01 15:07:07 -0300

    feat(molecules): adding banner components and image

--- src/Components/Pages/Home/index.tsx
+++ src/Components/Pages/Home/index.tsx
@@ -1,7 +1,15 @@
 import Main from '@/components/atoms/Main';
+import Banner from '@/components/molecules/Banner';
 
 const Home = () => {
-  return <Main></Main>;
+  return (
+    <Main>
+      <Banner
+        title='oferecemos os melhores serviços de segurança do trabalho e meio ambiente'
+        subtitle='segurança do trabalho'
+      />
+    </Main>
+  );
 };
 
 export default Home;
---
+++ src/Components/molecules/Banner/index.tsx
@@ -0,0 +1,33 @@
+import Section from '@/components/atoms/Section';
+import Text from '@/components/atoms/Text';
+
+type BannerProps = {
+  subtitle: string;
+  title: string;
+  customImage?: string;
+};
+
+const Banner = ({ subtitle, title, customImage }: BannerProps) => {
+  return (
+    <Section
+      style={{
+        backgroundImage: `url(assets/images/banners/home.jpg)`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+      className=' h-[calc(100vh-80px)]  md:h-[calc(100vh-102px)]'
+      childrenProps={{
+        className:
+          'text-center h-full w-full bg-gray-900/70 justify-center flex flex-col md:px-10 px-4 gap-6',
+      }}
+    >
+      <Text variant='subTitle'>{subtitle}</Text>
+      <Text tag='h1' variant='h1'>
+        {title}
+      </Text>
+    </Section>
+  );
+};
+
+export default Banner;
