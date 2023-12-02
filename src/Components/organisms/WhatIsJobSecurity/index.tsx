44a882f1cb3b8441660ab91fa9416cc8d3646012
Vítor <caulicons@hotmail.com>
2023-12-02 20:35:25 -0300

    revert(revert): revert to branch 992c3e6

--- src/Components/organisms/WhatIsJobSecurity/index.tsx
+++ src/Components/organisms/WhatIsJobSecurity/index.tsx
@@ -1,6 +1,6 @@
 import Section from '@/components/atoms/Section';
 import Text from '@/components/atoms/Text';
-import CardDropdown from '@/components/molecules/Cards/CardDropDown';
+import CardToggle from '@/components/molecules/Cards/CardToggle';
 import { whatIsJobSecurity } from '@data/infos';
 
 const WhatIsJobSecurity = () => {
@@ -15,13 +15,17 @@ const WhatIsJobSecurity = () => {
       <Text tag='h2' variant='h2'>
         o que é segurança do trabalho?
       </Text>
-      <div className='mt-4 flex flex-col justify-center gap-3 md:flex-row'>
+      <div className='mt-4 flex flex-col flex-wrap justify-center gap-3 md:flex-row lg:flex-row'>
         {whatIsJobSecurity.map((info) => (
-          <CardDropdown
+          <CardToggle
             key={info.title}
             title={info.title}
             description={info.description}
+<<<<<<< HEAD
             
+=======
+            className=''
+>>>>>>> parent of 992c3e6 (refactor(component/whatisjobsecurity): organizer classes)
           />
         ))}
       </div>
