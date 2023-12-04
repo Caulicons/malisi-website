8a4d70331ff508d6fedc999573b1a22da9400f38
Vítor <caulicons@hotmail.com>
2023-12-04 08:24:06 -0300

    feat(components): new about page and new section 'ourpurpose''

--- src/Components/Pages/About/index.tsx
+++ src/Components/Pages/About/index.tsx
@@ -1,5 +1,18 @@
+import Main from '@/components/atoms/Main';
+import Banner from '@/components/molecules/Banner';
+import OurPurpose from '@/components/organisms/OurPurpose';
+
 const About = () => {
-  return <div></div>;
+  return (
+    <Main>
+      <Banner
+        title='Aqui você certamente encontrará os melhores serviços de Engenharia de Segurança do Trabalho e Meio Ambiente. '
+        subtitle='segurança do trabalho'
+        height={50}
+      />
+      <OurPurpose />
+    </Main>
+  );
 };
 
 export default About;
---
+++ src/Components/organisms/OurPurpose/index.tsx
@@ -0,0 +1,45 @@
+import Section from '@/components/atoms/Section';
+import Text from '@/components/atoms/Text';
+import Card from '@/components/molecules/Cards';
+import { sectionOurPurpose } from '@data/infos';
+
+const OurPurpose = () => {
+  return (
+    <Section className='min-h-fit'>
+      <div className='flex max-w-sectionContainer flex-col gap-8'>
+        <div>
+          <Text
+            variant='subTitle'
+            className='tablet:text-start desktop:text-start'
+          >
+            {sectionOurPurpose.subTitle}
+          </Text>
+          <Text
+            tag='h2'
+            variant='h2'
+            className='text-secondary tablet:text-start desktop:text-start'
+          >
+            {sectionOurPurpose.title}
+          </Text>
+          <Text variant='p' className='mt-3 '>
+            {sectionOurPurpose.description}
+          </Text>
+        </div>
+        <div className='mx-auto flex flex-col flex-wrap gap-3 tablet:flex-row'>
+          {sectionOurPurpose.cards.map((card) => (
+            <Card
+              key={card.title}
+              title={card.title}
+              description={card.description}
+              icon={card.icon}
+              variant={card?.variant}
+              className='tablet:max-w-[49%]'
+            />
+          ))}
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default OurPurpose;
