c9f2ab0d2d7f17d03d27428504edc4a6d7aabe97
Vítor <caulicons@hotmail.com>
2023-12-07 09:06:24 -0300

    feat(install): jotai, state management

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  experimental: {
+    swcPlugins: [['@swc-jotai/react-refresh', {}]],
+  },
 };
 
 module.exports = nextConfig;
