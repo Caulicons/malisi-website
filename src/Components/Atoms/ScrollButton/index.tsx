6f1961dc6d93347a7940b2db610d523ee5e844c2
Vítor <caulicons@hotmail.com>
2023-12-14 08:18:27 -0300

    feat(componentsandassets): add scroll button and assets

---
+++ src/Components/Atoms/ScrollButton/index.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useState } from 'react';
+import { RiArrowUpDoubleFill } from 'react-icons/ri';
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 250) {
+      setVisible(true);
+    } else if (scrolled <= 250) {
+      setVisible(false);
+    }
+  };
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+  return (
+    <button
+      style={{ opacity: visible ? '1' : '0' }}
+      className='fixed bottom-5 left-8 rounded-xl bg-secondary p-3 shadow-2xl transition-all duration-1000 ease-in-out hover:bg-primary'
+      onClick={scrollToTop}
+    >
+      <div className='text-white'>
+        <RiArrowUpDoubleFill size={30} />
+      </div>
+    </button>
+  );
+};
+
+export default ScrollButton;
