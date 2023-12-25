dcdf307457d5842c22ed86a274812f8093eb1dae
Vítor <caulicons@hotmail.com>
2023-12-25 17:05:23 -0300

    fix(googleanalytics): refactor and start google analytics

--- src/Components/Atoms/Analytics/index.tsx
+++ src/Components/Atoms/Analytics/index.tsx
@@ -4,14 +4,14 @@ const Analytics = () => {
   return (
     <>
       <Script src='https://www.googletagmanager.com/gtag/js?id=G-WXHKFERECJ' />
-      <Script id='G-WXHKFERECJ'>
+      <Script id='google-analytics'>
         {`
-     window.dataLayer = window.dataLayer || [];
-     function gtag(){dataLayer.push(arguments);}
-     gtag('js', new Date());
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
    
-     gtag('config', 'G-WXHKFERECJ');
-    `}
+          gtag('config', 'G-WXHKFERECJ');
+        `}
       </Script>
     </>
   );
