1b498d7a18f0cd5bc1e596d4dc1b556deddd72e7
Vítor <caulicons@hotmail.com>
2023-12-26 08:43:07 -0300

    refactor(analytics): create a .env.local to isolate the id key

--- src/Components/Atoms/Analytics/index.tsx
+++ src/Components/Atoms/Analytics/index.tsx
@@ -3,14 +3,17 @@ import Script from 'next/script';
 const Analytics = () => {
   return (
     <>
-      <Script src='https://www.googletagmanager.com/gtag/js?id=G-WXHKFERECJ' />
-      <Script id='google-analytics'>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script strategy='lazyOnload' id='google-analytics'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
+          gtag('js', new Date());
    
-          gtag('config', 'G-WXHKFERECJ');
+          gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
         `}
       </Script>
     </>
