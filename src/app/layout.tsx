43015b9fadc6c999a039e45294376c04e0b03b25
Vítor <caulicons@hotmail.com>
2023-12-26 11:46:59 -0300

    feat(pwa implementation): add progressive web app functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,21 @@ import { Inter } from 'next/font/google';
 import Header from '@/components/organisms/Header';
 import Footer from '@/components/organisms/Footer';
 import Analytics from '@/components/atoms/Analytics';
+import { Viewport } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const viewport: Viewport = {
+  themeColor: '#147f3c',
+};
 
 export const metadata: Metadata = {
   title: 'Malisi',
   description:
     'Malisi, oferecemos a você os melhores serviços de segurança do trabalho e meio ambiente.',
+  manifest: '/manifest.json',
   icons: {
-    icon: '/assets/images/favicon/index.png',
+    icon: '/assets/images/favicon/256x256.png',
   },
 };
 
@@ -24,11 +30,9 @@ export default function RootLayout({
   return (
     <html lang='pt-br'>
       <Analytics />
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
+      <Header />
+      <body className={inter.className}>{children}</body>
+      <Footer />
     </html>
   );
 }
